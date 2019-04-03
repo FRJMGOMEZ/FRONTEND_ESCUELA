263db76be524d176c96975009a32f81bba931a3d
frjma <frjmartinezgomez@gmail.com>
2019-04-03 18:24:24 +0200

    First deploy cargomusicadm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,4 +20,4 @@ const ROUTES: Routes = [
 ];
 
 
-export const APPROUTES = RouterModule.forRoot(ROUTES, {initialNavigation: 'enabled',paramsInheritanceStrategy: 'always',useHash: true});
+export const APPROUTES = RouterModule.forRoot(ROUTES, {initialNavigation: 'enabled',paramsInheritanceStrategy: 'always',useHash: false});
