4a4066f3b8ca54ae929255fb1baa3e147bc8f3bf
frjma <frjmartinezgomez@gmail.com>
2019-04-09 10:50:20 +0200

    Subida demo a producción

--- src/app/pipes/files.pipe.ts
+++ src/app/pipes/files.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { FileModel } from '../models/file.model';
+import * as fs from "fs";
 
 @Pipe({
   name: 'filesPipe',
@@ -8,7 +9,12 @@ import { FileModel } from '../models/file.model';
 export class FilesPipe implements PipeTransform {
 async  transform(file:FileModel) {
     let buff = await new Buffer(file['file'].data)
-    let base64data = await buff.toString('base64');
-    return base64data
+    let base64data = await buff.toString('base64')
+    base64data = base64data.split(';base64,').pop();
+    let pdf = await fs.writeFile('image.png', base64data, { encoding: 'base64' }, function (err) {
+    if(err){console.log(err)}
+  });
+  console.log(pdf)
+    return pdf
   }
 }
