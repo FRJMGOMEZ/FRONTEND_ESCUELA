19f486212eff0a2b8361ef2c023ee81ebe14bb52
frjma <frjmartinezgomez@gmail.com>
2019-09-24 10:35:23 +0200

    Diseño responsive por tamaño de pantalla añadido

--- src/app/pipes/files.pipe.ts
+++ src/app/pipes/files.pipe.ts
@@ -2,15 +2,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { URL_SERVICES } from '../config/config';
 import { FileModel } from '../models/file.model';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Pipe({
   name: 'filesPipe',
   pure:true
 })
 export class FilesPipe implements PipeTransform {
+
+  constructor(public http:HttpClient){}
   transform(file:FileModel) {
     if(environment.production){
-      return file.location
+      return this.http.get(file.location)
     }else{
       let url = `${URL_SERVICES}files/${file.type}/${file.name}`;
       return url
