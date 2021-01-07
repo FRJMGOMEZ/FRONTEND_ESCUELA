0c1ce3799c2685af4d9768936c8bdc8cd397f347
FRJMGOMEZ <frjmartinezgomez@gmail.com>
2021-01-07 17:44:30 +0100

    indexcard model restored

--- src/app/models/indexCard.model.ts
+++ src/app/models/indexCard.model.ts
@@ -1,13 +1,13 @@
 export class Indexcard {
 
-constructor(
-            public role: string,
-            public name:string,
-            public surname?:string,
-            public email?:string,
-            public mobile?:string,
-            public home?:string,
-            public address?:string,
-            public _id?:string){
-}
+    constructor(
+        public role: string,
+        public name: string,
+        public surname?: string,
+        public email?: string,
+        public mobile?: string,
+        public home?: string,
+        public address?: string,
+        public _id?: string) {
+    }
 }
