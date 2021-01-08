915c6534cf1496f17158da78fd76104a4ae53dfe
unknown <frjmartinezgomez@gmail.com>
2021-01-08 18:24:27 +0100

    changes in package.json

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,7 +2,6 @@ import { environment } from '../../environments/environment';
 
 export const URL_SERVICES = environment.baseUrl;
 
-console.log(URL_SERVICES)
 
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl:'https://cargomusicadm.herokuapp.com/api/'
+  baseUrl:'https://cargomusicadm.herokuapp.com/api'
 };
