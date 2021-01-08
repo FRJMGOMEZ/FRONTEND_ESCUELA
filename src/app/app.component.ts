63c8dd58c18e4a0ee6d326c8b11dcf529470a9f7
unknown <frjmartinezgomez@gmail.com>
2021-01-08 19:08:51 +0100

    another api used to test2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserServices } from './providers/user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+    constructor(private userService:UserServices){
+      this.userService.getStarwarsCharacters().subscribe()
+    }
+
+      
   
 }
